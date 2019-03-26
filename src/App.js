ad968eee0b57942fa1d2da3db66daed872642692
amdad <amdadul.haque@EkkBaz.com>
2019-03-26 11:42:55 +0600

    massage

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React Pharmecy </h2>
+          <h2>Welcome to Riaz vai yor website is almost done </h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, <code>pass me the content of your company</code> and i
+          would love to create this website .
         </p>
       </div>
     );
